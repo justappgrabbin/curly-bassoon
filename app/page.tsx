7986c6adb3cb060a847fee164056e02ff233bab1
justappgrabbin <bookingalexismcmillion@gmail.com>
2026-03-23 05:20:13 -0700

    Add Dockerfile for Python app with Ollama

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { SentientCore } from '@/components/SentientCore';
+
+export default function Home() {
+  return (
+    <main className="relative w-full h-screen overflow-hidden bg-void">
+      <SentientCore />
+    </main>
+  );
+}
